8e351406b1468f36ea3122aa75da0903d311d9ff
AirinNila <AirinNila>
2025-07-01 22:12:04 +0600

    updated coffee

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -37,6 +37,41 @@ async function run() {
     app.get('/coffee', async (req, res) => {
       const cursor = coffeeCollection.find();
       const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.get('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.put('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      updateCoffe = req.body;
+      const coffee = {
+        $set: {
+          name: updateCoffe.name,
+          supplier: updateCoffe.supplier,
+          category: updateCoffe.category,
+          cafe: updateCoffe.cafe,
+          test: updateCoffe.test,
+          details: updateCoffe.details,
+          photo: updateCoffe
+                  }
+      }
+
+      const result = await coffeeCollection.updateOne(filter, coffee, options)
+      res.send(result)
+    })
+
+    app.delete('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await coffeeCollection.deleteOne(query);
       res.send(result)
     })
 
